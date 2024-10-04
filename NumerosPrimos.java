5e6d91a05e8de2e3a5bd06dacb610d777156c518
SCIS3-PC02\SCIS3-PC02 <dandre91y_x792c@tigpe.com>
2024-10-04 13:26:54 -0500

    Resultado final

---
+++ NumerosPrimos.java
@@ -0,0 +1,20 @@
+public class NumerosPrimos {
+    public static void main(String[] args) {
+        System.out.println("Números primos del 1 al 100:");
+
+        for (int num = 2; num <= 100; num++) {
+            boolean esPrimo = true;
+
+            for (int i = 2; i <= Math.sqrt(num); i++) {
+                if (num % i == 0) {
+                    esPrimo = false;
+                    break;
+                }
+            }
+
+            if (esPrimo) {
+                System.out.print(num + " ");
+            }
+        }
+    }
+}
